718635ed62f028f39929aa2209f9112305951107
Rituraj Singh <riturajsinghr97@gmail.com>
2025-12-14 00:39:52 +0530

    Auto create superuser script

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python core_service/manage.py collectstatic --no-input
 
 # 3. Migrate Database
 python core_service/manage.py migrate
+
+# 4. Create Superuser 
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'pass123')" | python core_service/manage.py shell
